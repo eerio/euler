a02df9537683c0741d5cde674614ecd36736f944
Pawel Balawender <pbalawender101@gmail.com>
2020-03-21 20:13:02 +0100

    Add a script to automatically upload a solution

---
+++ submit_answer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+file="response.html"
+
+read -p "Type problem number:" prob
+read -p "Type answer:" ans
+
+tok=$(./get_submit_token.sh $prob)
+
+./fetch_captcha.sh
+read -p "Solve captcha:" cap
+
+curl https://projecteuler.net/problem=$prob \
+	-H @headers.txt \
+	-d "guess_$prob=$ans&submit_token=$tok&captcha=$cap" \
+	--compressed -Ls -o $file
+
+if grep -q "<p>Sorry, but the answer you gave appears to be incorrect.</p>" $file; then
+	echo "Incorrect!"
+elif grep -q "<p>Congratulations, the answer you gave to problem $prob is correct.</p>" $file; then
+	echo "Correct!"
+elif grep -q "An error occurred whilst trying to submit your guess<br>Please try again" $file; then
+	echo "An error occured :("
+fi
+
