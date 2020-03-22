905a98b8f987f0435fe6475d7a62abe54cc3e589
Pawel Balawender <pbalawender101@gmail.com>
2020-03-22 23:47:02 +0100

    Add error handling

--- login.sh
+++ login.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 file="credentials.txt"
+out_file="response.html"
+
 user="$(head -1 $file | head -c -1)"
 pass="$(tail -1 $file | head -c -1)"
 
@@ -10,5 +12,12 @@ curl https://projecteuler.net/captcha/show_captcha.php?0 \
 read -p "Enter captcha:" cap
 
 curl https://projecteuler.net/sign_in \
-	--cookie cookies  -L -o /dev/null -s \
+	--cookie cookies  -L -o $out_file -s \
 	-d "username=$user&password=$pass&captcha=$cap&sign_in=Sign+In"
+
+if grep -q "Sign in successful" $out_file; then
+	exit 0
+else
+	echo "Something went wrong. Look at the content of $out_file"
+	exit 1
+fi
