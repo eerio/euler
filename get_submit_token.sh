5db8738c42f223e16a00f5cd4a69f8551e0b8d98
Pawel Balawender <pbalawender101@gmail.com>
2020-03-22 18:41:55 +0100

    Check if output empty and if so, ask if the problem hasnt been solved already

--- get_submit_token.sh
+++ get_submit_token.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-curl -s -H @headers.txt https://projecteuler.net/problem=$1 --compressed \
-	| sed 's/.*name=\"submit_token\" value=\"\([0-9a-z]*\)\".*/\1/p' -n
+curl https://projecteuler.net/problem=$1 \
+	--compressed -s -H @headers.txt --cookie cookies \
+	| sed 's/.*name=\"submit_token\" value=\"\([0-9a-z]*\)\".*/\1/p' -n \
+	| ifne -n echo "Havent you already submitted the answer?"
+
