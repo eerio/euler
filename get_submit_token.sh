1ad01cc051e2bbb64ab67a7ac485834f9b864da8
Pawel Balawender <pbalawender101@gmail.com>
2020-03-22 23:46:04 +0100

    Return a proper exit code

--- get_submit_token.sh
+++ get_submit_token.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-curl https://projecteuler.net/problem=$1 \
+result=$(curl https://projecteuler.net/problem="$1" \
 	--compressed -s -H @headers.txt --cookie cookies \
-	| sed 's/.*name=\"submit_token\" value=\"\([0-9a-z]*\)\".*/\1/p' -n \
-	| ifne -n echo "Havent you already submitted the answer?"
+	| sed 's/.*name=\"submit_token\" value=\"\([0-9a-z]*\)\".*/\1/p' -n)
+
+if [ -z "$result" ]
+then
+	echo "Either you're logged out or have already solved this problem"
+	exit 1
+else
+	echo "$result"
+	exit 0
+fi
 
