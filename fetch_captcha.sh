227dcc016bf7a9053834f14be143b4c8d41554c9
Pawel Balawender <pbalawender101@gmail.com>
2020-03-23 00:19:28 +0100

    Put url to variable

--- fetch_captcha.sh
+++ fetch_captcha.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-file_cookie=cookies
-file_headers=headers.txt
-file_captcha=captcha.png
+file_cookie="cookies"
+file_headers="headers.txt"
+file_captcha="captcha.png"
+euler="https://projecteuler.net"
 
-curl -s https://projecteuler.net/captcha/show_captcha.php?0 \
+curl -s "$euler/captcha/show_captcha.php?" \
 	-H @"$file_headers" --cookie "$file_cookie" -o "$file_captcha"
