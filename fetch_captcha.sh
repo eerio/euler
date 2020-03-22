56b2e4ee13986dab16c4e3b916ebe938d01a1ccc
Pawel Balawender <pbalawender101@gmail.com>
2020-03-23 00:11:16 +0100

    Fix a problem with invalid captcha, just use a cookie in captcha fetching

--- fetch_captcha.sh
+++ fetch_captcha.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+file_cookie=cookies
+file_headers=headers.txt
+file_captcha=captcha.png
+
 curl -s https://projecteuler.net/captcha/show_captcha.php?0 \
-	-H @headers.txt -o captcha.png
+	-H @"$file_headers" --cookie "$file_cookie" -o "$file_captcha"
