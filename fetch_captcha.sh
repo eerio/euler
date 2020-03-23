4534b5492beabd55d6c3f181d729cc9e75528e57
Pawel Balawender <pbalawender101@gmail.com>
2020-03-23 01:10:18 +0100

    Fix invalid variable name

--- fetch_captcha.sh
+++ fetch_captcha.sh
@@ -2,4 +2,4 @@
 source config.sh
 
 curl -s "$euler/captcha/show_captcha.php?" \
-	-H @"$file_headers" --cookie "$file_cookie" -o "$file_captcha"
+	-H @"$file_headers" --cookie "$file_cookies" -o "$file_captcha"
