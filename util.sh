ed1c8ce9a2f7168e9ad15b2d461f2c37153307f3
Pawel Balawender <eerio@debian-vm.pl>
2020-04-14 21:00:29 +0200

    move

--- util.sh
+++ util.sh
@@ -43,3 +43,27 @@ get_submit_token() {
 	fi
 }
 
+login() {
+	user="$(head -1 $file_credentials | head -c -1)"
+	pass="$(tail -1 $file_credentials | head -c -1)"
+
+	curl "$euler/captcha/show_captcha.php?" \
+	        --cookie-jar "$file_cookies" -o "$file_captcha" -s
+
+	./view_captcha.sh
+
+	read -p "Enter captcha:" cap
+
+	curl "$euler/sign_in" \
+	        --cookie "$file_cookies"  -L -o "$file_html" -s \
+		-d "username=$user&password=$pass&captcha=$cap&sign_in=Sign+In"
+
+	if grep -q "Sign in successful" "$file_html"; then
+	        echo "Sign in successful"
+		exit 0
+	else
+		echo "Something went wrong. Look at the content of $out_file"
+		exit 1
+	fi
+}
+
