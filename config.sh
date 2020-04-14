503f86ca55a0e8f6b7fdd536763ac738580759e9
Pawel Balawender <eerio@debian-vm.pl>
2020-04-14 21:41:36 +0200

    Move

--- config.sh
+++
@@ -1,7 +0,0 @@
-euler="https://projecteuler.net"
-file_html="response.html"
-file_headers="headers.txt"
-file_cookies="cookies"
-file_captcha="captcha.png"
-file_credentials="credentials.txt"
-captcha_jpg="${file_captcha%.png}.jpg"
