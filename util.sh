d96be33ea5c092dcb4b5ea15862dfd7487685551
Pawel Balawender <eerio@debian-vm.pl>
2020-04-14 20:59:14 +0200

    move

--- util.sh
+++ util.sh
@@ -28,3 +28,18 @@ get_question() {
 		| sed 's/<[^>]*>//g'
 }
 
+get_submit_token() {
+	result=$(curl "$euler/problem=$1" \
+		--compressed -s -H @"$file_headers" --cookie "$file_cookies" \
+		| sed 's/.*name=\"submit_token\" value=\"\([0-9a-z]*\)\".*/\1/p' -n)
+	
+	if [ -z "$result" ]
+	then
+	        echo "Either you're logged out or have already solved this problem"
+		exit 1
+	else
+		echo "$result"
+		exit 0
+	fi
+}
+
