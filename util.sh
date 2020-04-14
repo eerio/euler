d08c6df06a63f058fea240358266a9315ba8d966
Pawel Balawender <eerio@debian-vm.pl>
2020-04-14 20:47:24 +0200

    move view_captcha to util as a functin

--- util.sh
+++ util.sh
@@ -5,3 +5,8 @@ clear() {
 	rm "$file_captcha" "$captcha_jpg" "$file_html"
 }
 
+view_captcha() {
+	convert "$file_captcha" "$captcha_jpg"
+	jp2a "$captcha_jpg" -z --color --chars='..00xx@@'
+}
+
--- view_captcha.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source config.sh
-
-convert "$file_captcha" "$captcha_jpg"
-jp2a "$captcha_jpg" -z --color --chars='..00xx@@'
-
